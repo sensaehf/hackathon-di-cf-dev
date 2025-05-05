339cd946e2e38fa25e2ae084f9707a51ae8bd114
snaits <stian.lie-sveen@inmeta.no>
2025-05-05 10:38:38 +0000

    Fix tests!

--- apps/cf-prep/api/src/app/modules/consent/consent.controller.spec.ts
+++ apps/cf-prep/api/src/app/modules/consent/consent.controller.spec.ts
@@ -2,21 +2,56 @@ import { Test, TestingModule } from '@nestjs/testing'
 
 import { ConsentController } from './consent.controller'
 import { ConsentService } from './consent.service'
+import { Consent } from './consent.model'
 
 describe('AppController', () => {
   let app: TestingModule
 
+  interface Then {
+    result: Consent | null
+    error: Error
+  }
+
+  type GivenWhenThen = () => Promise<Then>
+  let then : Then
+  let givenWhenThen: GivenWhenThen
+
   beforeAll(async () => {
+
+    const repositoryMock = {  
+      findByNationalId: jest.fn().mockResolvedValue({ nationalId: "1", consented: true }),      
+    };  
+        
+    givenWhenThen = async () => {      
+
+      const then = {} as Then
+      await app.get<ConsentController>(ConsentController).getData()
+        .then((result) => (then.result = result))
+        .catch((error) => (then.error = error))
+      
+      return then
+    }
+
     app = await Test.createTestingModule({
       controllers: [ConsentController],
-      providers: [ConsentService],
+      providers: [{
+        provide: ConsentService,  
+        useValue: repositoryMock
+      }],
     }).compile()
+
+   
+    
+       
+       
   })
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
-      const appController = app.get<ConsentController>(ConsentController)
-      expect(appController.getData()).toEqual({ message: 'Hello API' })
+    it('should return "Hello API"', async () => {
+      
+      const then = await givenWhenThen()
+      
+      expect(then.result).toEqual({ nationalId: "1", consented: true })
     })
   })
 })
--- apps/cf-prep/api/src/app/modules/consent/consent.service.spec.ts
+++ apps/cf-prep/api/src/app/modules/consent/consent.service.spec.ts
@@ -1,21 +1,78 @@
+import { mock } from 'jest-mock-extended'
 import { Test } from '@nestjs/testing'
 
 import { ConsentService } from './consent.service'
+import { LOGGER_PROVIDER } from '@island.is/logging'
+import { Consent } from './consent.model'
+import { getModelToken } from '@nestjs/sequelize'
 
 describe('AppService', () => {
   let service: ConsentService
 
-  beforeAll(async () => {
-    const app = await Test.createTestingModule({
-      providers: [ConsentService],
-    }).compile()
+  interface Then {
+    result: Consent | null
+    error: Error
+  }
 
-    service = app.get<ConsentService>(ConsentService)
+  type GivenWhenThen = () => Promise<Then>
+  let givenWhenThen: GivenWhenThen
+
+  beforeAll(async () => {
+
+    const repositoryMock = {  
+      findOne: jest.fn().mockResolvedValue({ nationalId: "1", consented: true }),  
+      findAll: jest.fn().mockResolvedValue([{ nationalId: "1", consented: true }]),  
+      create: jest.fn().mockResolvedValue({ nationalId: "1", consented: true }),  
+      update: jest.fn(),  
+      destroy: jest.fn().mockResolvedValue(1),  
+    };  
+
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        {
+          provide: getModelToken(Consent),  
+          useValue: repositoryMock
+        },
+        {  
+          provide: LOGGER_PROVIDER,  
+          useValue: {  
+            info: jest.fn(),  
+            error: jest.fn(),
+            debug: jest.fn()
+          },  
+        },        
+        ConsentService,
+      ],
+    })
+    .useMocker((token) => {
+      if (typeof token === 'function') {
+        return mock()
+      }
+    })
+    .compile()
+
+    service = moduleRef.get<ConsentService>(ConsentService)
+
+    
+    givenWhenThen = async () => {
+      const then = {} as Then
+
+      await 
+        service.findByNationalId('1')
+        .then((result) => (then.result = result))
+        .catch((error) => (then.error = error))
+
+      return then
+    }
+    
+
   })
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
-      expect(service.findByNationalId('1234567890')).toEqual({ message: 'Hello API' })
+    it('should return "Hello API"', async () => {
+      const then = await givenWhenThen()
+
+      expect(then.result).toEqual({ nationalId: "1", consented: true })
     })
   })
 })
