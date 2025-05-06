d4916ae0a8c8c47f1819961315ada62f529f1aa5
snaits <stian.lie-sveen@inmeta.no>
2025-05-06 21:41:39 +0000

    Merge app.module.ts

--- apps/tax-return/backend/src/app/app.module.ts
+++ apps/tax-return/backend/src/app/app.module.ts
@@ -7,11 +7,8 @@ import { SequelizeConfigService } from './sequelizeConfig.service'
 import { SalaryModule } from '../modules/salary/salary.module'
 import { PerkModule } from '../modules/perks/perk.module'
 import { SubsidyModule } from '../modules/subsidy/subsidy.module'
-<<<<<<< HEAD
 import { OtherReliabilitiesModule } from '../modules/other-reliabilities/other-reliabilities.module'
-=======
 import { VehicleModule } from '../modules/vehicle/vehicle.module'
->>>>>>> 527ea937a2 (TADI-107 GET Vehicles for tax submission id)
 
 @Module({
   imports: [
@@ -23,11 +20,8 @@ import { VehicleModule } from '../modules/vehicle/vehicle.module'
     PerkModule,
     SalaryModule,
     SubsidyModule,
-<<<<<<< HEAD
-    OtherReliabilitiesModule
-=======
+    OtherReliabilitiesModule,
     VehicleModule
->>>>>>> 527ea937a2 (TADI-107 GET Vehicles for tax submission id)
   ]  
 })
 export class AppModule {}
