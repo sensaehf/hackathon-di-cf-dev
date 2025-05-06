3a3b02be6605b27de205fc7ac1328ba4962302e3
Nathanael Dungca <60610773+natdungc@users.noreply.github.com>
2025-05-06 17:52:43 +0000

    fix layouts and add login page (#28)

---
+++ apps/tax-return/web/layouts/MainLayout.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Head from 'next/head'
+import {
+  Button,
+  Footer,
+  GridContainer,
+  Header,
+} from '@island.is/island-ui/core'
+import { useRouter } from 'next/router'
+
+import en from '../public/locales/en/common.json'
+import is from '../public/locales/is/common.json'
+
+const translations: any = { en, is }
+
+export const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  const { locale = 'en', push, pathname, query, asPath } = useRouter()
+
+  const t = translations[locale] || translations.en
+
+  const thisLocale = locale === 'en' ? 'IS' : 'EN'
+  const headerItems = () => (
+    <>
+      <Button variant="utility" icon={'person'}>
+        {t.myPages}
+      </Button>
+      <Button
+        variant="utility"
+        onClick={() =>
+          push({ pathname, query }, asPath, {
+            locale: `${locale.toLowerCase() === 'en' ? 'is' : 'en'}`,
+          })
+        }
+      >
+        {thisLocale}
+      </Button>
+      <Button variant="utility" icon={'menu'}>
+        {t.menu}
+      </Button>
+    </>
+  )
+
+  return (
+    <>
+      <Head>
+        <title>Island.is - Tax returns</title>
+      </Head>
+      <>
+        <header>
+          <GridContainer>
+            <Header headerItems={headerItems()} />
+          </GridContainer>
+        </header>
+        {children}
+        <Footer />
+      </>
+    </>
+  )
+}
--- apps/tax-return/web/pages/_app.tsx
+++ apps/tax-return/web/pages/_app.tsx
@@ -1,65 +1,23 @@
 import { AppProps } from 'next/app'
-import Head from 'next/head'
 import './styles.css'
-import {
-  ActionCard,
-  Box,
-  Button,
-  Footer,
-  GridContainer,
-  Header,
-  Menu,
-} from '@island.is/island-ui/core'
-import { useRouter } from 'next/router'
 
 import en from '../public/locales/en/common.json'
 import is from '../public/locales/is/common.json'
+import mockUser from '../mock/mockUser.json'
+import React from 'react'
 
 const translations: any = { en, is }
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  const { locale = 'en', push, pathname, query, asPath } = useRouter()
-
-  const t = translations[locale] || translations.en
-
-  const thisLocale = locale === 'en' ? 'IS' : 'EN'
-  const headerItems = () => (
-    <>
-      <Button variant="utility" icon={'person'}>
-        {t.myPages}
-      </Button>
-      <Button
-        variant="utility"
-        onClick={() =>
-          push({ pathname, query }, asPath, {
-            locale: `${locale.toLowerCase() === 'en' ? 'is' : 'en'}`,
-          })
-        }
-      >
-        {thisLocale}
-      </Button>
-      <Button variant="utility" icon={'menu'}>
-        {t.menu}
-      </Button>
-    </>
-  )
-
-  return (
-    <>
-      <Head>
-        <title>Island.is - Tax returns</title>
-      </Head>
-      <>
-        <header>
-          <GridContainer>
-            <Header headerItems={headerItems()} />
-          </GridContainer>
-        </header>
-        <Component {...pageProps} />
-        <Footer />
-      </>
-    </>
-  )
+type CustomAppProps = AppProps & {
+  Component: AppProps['Component'] & {
+    getLayout?: (page: React.ReactNode) => React.ReactNode
+  }
 }
 
-export default CustomApp
+function App({ Component, pageProps }: CustomAppProps) {
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
+}
+
+export default App
